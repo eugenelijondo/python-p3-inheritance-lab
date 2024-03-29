bf96924e14f85254f496d2ce4770d527a350b264
eugenelijondo <eugene.lijondo@student.moringaschool.com>
2024-03-29 11:53:32 +0300

    first commit

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,8 @@ from user import User
 import random
 
 class Teacher(User):
-
+ def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
     def teach(self):
         pass
